b7130312141976f52780bd5a7ace9a1461327591
Maria Popova <0979867251m@gmail.com>
2023-06-03 13:11:03 +0300

    finish

--- contacts.js
+++ contacts.js
@@ -1,2 +1,47 @@
 const fs = require("fs").promises;
+const { nanoid } = require("nanoid");
 const path = require("path");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+console.log(contactsPath);
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const data = await listContacts();
+  const result = data.find((item) => item.id === contactId);
+  return result || null;
+}
+
+async function addContact({ name, email, phone }) {
+  const contact = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contact.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
+  return newContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
